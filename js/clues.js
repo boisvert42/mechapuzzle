bbb84c48f648bd6ffa687358cca6620a93b5ce3a
Alex Boisvert <boisvert42@gmail.com>
2020-11-22 10:05:55 -0800

    Allow for clicking on "clue" bars

--- js/clues.js
+++ js/clues.js
@@ -10,6 +10,18 @@ function isLetter(str) {
     return str.length === 1 && str.match(/[a-z]/i);
 }
 
+function clues_of_length(arr)
+{
+    // Display clues of a given length in the #clues1 div
+    var html = '<big><pre>\n';
+    for (var j = 0; j < arr.length; j++) {
+        var entry = arr[j][0]; var clue = arr[j][1];
+        html += ' [ ' + clue + ' ] : ' + entry + ' ]\n';
+    }
+    html += '</pre></big>\n';
+    document.getElementById('clues1').innerHTML = html;
+}
+
 function clue_initial_letters() {
     var puzdata = PUZAPP.puzdata;
     var letters = [];
@@ -36,14 +48,17 @@ function clue_lengths() {
     // Display the number of clues for each length
     var puzdata = PUZAPP.puzdata;
     var clue_lists = [puzdata.across_clues, puzdata.down_clues];
+	var entry_lists = [puzdata.across_entries, puzdata.down_entries];
     var categories = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '>= 10'];
     var data = [];
+	var clues_by_count = {};
     // Initialize the data
     for (var i = 0; i < categories.length; i++) {
         data.push(0);
     }
     for (var j = 0; j < clue_lists.length; j++) {
         var clues = clue_lists[j];
+		var entries = entry_lists[j];
         for (var key in clues) {
             if (!clues.hasOwnProperty(key))
                 continue;
@@ -53,6 +68,11 @@ function clue_lengths() {
                 clue_length = 10;
             // Push to "data"
             data[clue_length - 1] += 1;
+			// Push to clues_by_count
+			if (!clues_by_count[clue_length]) {
+                clues_by_count[clue_length] = [];
+            }
+			clues_by_count[clue_length].push([entries[key], clues[key]]);
         }
     }
     data.unshift('Count');
@@ -65,7 +85,8 @@ function clue_lengths() {
         data: {
             columns: [data],
             type: 'bar',
-            labels: true
+            labels: true,
+			onclick: function (e) { clues_of_length(clues_by_count[e.index + 1]); }
         },
         size: {
             // Chart won't render unless we initialize the size up front
